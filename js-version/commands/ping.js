f52fc7126a2ac333f04df8ae72c0350b80a94f48
Michael Barnes <42668288+mhbarnes@users.noreply.github.com>
2021-09-15 18:46:35 -0700

    Delete js-version/commands directory

--- js-version/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Basic test command. Replies with \"pong\".',
-    execute(msg, args) {
-      msg.reply('pong');
-      msg.channel.send('pong');
-    },
-  };
