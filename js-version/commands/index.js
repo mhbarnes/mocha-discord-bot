43b73a2d15cdec3c5051a8b691cfc781d7aab998
Michael Barnes <michaelbarnes822@gmail.com>
2021-09-15 18:47:40 -0700

    Deleted js-files.

--- js-version/commands/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    Ping: require('./ping'),
-    Shutdown: require('./shutdown')
-};
-// ,
-//     Popname: require('./popname'),
-//     
-//     Reply: require('./reply'),
-//     Leaguecheck: require('./leaguecheck')
--- js-version/commands/reply.js
+++
@@ -1,6 +0,0 @@
-/*------------------------------------------------------------------------------
-- Command: reply
-- Usage: !reply
-- Description: Replies with a message generated from a predetermined list. Can
--   be executed by anyone with a role higher than @everyone. 
-------------------------------------------------------------------------------*/
